4e5d680fd1189389d56ad06386a77f7b8a4cc660
Khasreto <adam-krawczyk@outlook.com>
2021-04-16 11:13:54 +0200

    update panther_sim

--- panther_simulation_ros1/build_and_run.sh
+++ panther_simulation_ros1/build_and_run.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
-echo "To re-run use command docker start -i rap"
+echo "To re-run use command docker start -i panther_sim"
 
 xhost +local:root
 
-docker build -q --tag panther:1.0 -t panther_sim .
+docker build --tag panther:1.0 -t panther_sim .
 
-docker run --net=host -e ROS_MASTER_URI -it --env="DISPLAY" --env="QT_X11_NO_MITSHM=1" -v /tmp/.X11-unix:/tmp/.X11-unix --name panther_sim panther_sim ./panther.bash
+docker run --net=host -it \
+    --env="DISPLAY" \
+    --runtime=nvidia \
+    --env="QT_X11_NO_MITSHM=1" \
+    --env=NVIDIA_VISIBLE_DEVICES=all \
+    --env=NVIDIA_DRIVER_CAPABILITIES=all \
+    -v /tmp/.X11-unix:/tmp/.X11-unix \
+    --name panther_sim panther_sim \
+    ./panther.bash
 
 xhost -local:root
